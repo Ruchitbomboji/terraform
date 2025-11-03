a79720028b3c59384757b2a614c5e460b0f9799b
Ruchitbomboji <ruchitb27@gmail.com>
2025-11-03 13:06:57 +0530

    day-4

--- day-4-state_lock/backend.tf
+++ day-4-state_lock/backend.tf
@@ -1,7 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "ruchit-s3-buckettt"
-    key    = "day-04/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "ruchit-s3-buckettt"
+    key            = "day-4/terraform.tfstate"
+    region         = "us-east-1"
+    use_lockfile = true
+    #dynamodb_table = "dynamodb1"
+    #encrypt = true
   }
 }
