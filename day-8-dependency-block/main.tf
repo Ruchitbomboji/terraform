984d10d920a3504d939315e35b1a9e72d34bdb33
Ruchitbomboji <ruchitb27@gmail.com>
2025-11-11 12:25:21 +0530

    day-8-dependency-block

---
+++ day-8-dependency-block/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "name" {
+  ami = "ami-07860a2d7eb515d9a"
+  instance_type = "t3.micro"
+}
+
+resource "aws_s3_bucket" "name" {
+  bucket = "asdfghjkloiujhbvcxswerfgbhnhuyh"
+  depends_on = [ aws_instance.name ]
+}
