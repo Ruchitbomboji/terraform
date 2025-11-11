c074954f65e4ce37d5dcee273ef606761ad04c9e
Ruchitbomboji <ruchitb27@gmail.com>
2025-11-11 13:21:59 +0530

    day-11-terraform-workspace

---
+++ day-11-terraform-workspace/main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "name" {
+    bucket = "asdfghjkokmnbvcdswerfghjiuyhgvcxswerfgb"
+  
+}
+
+#terraform workspace new <name of workspace> ---creates the workspace
+#terraform workspace list ---shows the list of workspace
+#terraform workspace select <name of workspace>---switches to the selected workspace
+#terraform workspace show---shows the current workspace
